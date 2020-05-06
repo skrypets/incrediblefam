d5812b36c0e424624396a48d97cd431e08cc903d
Mykola Skrypets <mykola.skrypets@lebara.com>
2020-05-06 19:17:02 +0300

    small changes

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Incrediblefam created for fun</strong> by
+          <a href="https://github.com/skrypets">Mykola Skrypets</a>.
+        </p>
+        <p class="is-italic">
+          Stack used:
+          <a href="https://www.nuxtjs.org/">Nuxt.js</a>,
+          <a href="https://www.contentful.com/">Contentful</a>,
+          <a href="https://aws.amazon.com/ru/amplify/">AWS Amplify</a>,
+          <a href="https://analytics.google.com/analytics/web/">Google Analytics</a> ❤️
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
